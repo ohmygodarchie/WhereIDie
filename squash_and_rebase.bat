84354b21e7fd4c1fd42209a6a0820a786e8ef2e0
Archit Kalla <archkalla@gmail.com>
2021-06-11 16:46:34 -0400

    somethings

---
+++ squash_and_rebase.bat
@@ -0,0 +1,26 @@
+@ECHO OFF
+TITLE Squash and Rebase
+ECHO This will Squash your work into one commit in the git
+::ECHO ONLY PROCEED IF YOU HAVE TESTED ::
+PAUSE
+ECHO ENTER THE BRANCH NAME YOU ARE WORKING ON EXACTLY
+SET branchname="ENTER BRANCH NAME: "
+PAUSE
+::get last commit SHA::
+last_commit_SHA=$(git rev-parse HEAD)
+::squash into 1 commit::
+git rebase -i %last_commit_SHA%
+::push to remote::
+git push origin %branchname% --force
+
+ECHO IF ALL YOU WANT TO DO IS SQUASH THEN DO NOT PROCEED AND EXIT
+ECHO IF YOU PROCEED YOU WILL COMMIT AND MERGE TO MASTER
+PAUSE
+git checkout master
+git pull origin master
+git checkout %branchname%
+git rebase master
+git push origin %branchname% --force
+git cehckout master
+git merge %branchname%
+git push origin master
