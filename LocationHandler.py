89855c04a7b3cb0dd69c747fe950c9d9b41d3803
Archit Kalla <archkalla@gmail.com>
2021-12-29 02:43:49 -0500

    added location handling and round class

---
+++ LocationHandler.py
@@ -0,0 +1,21 @@
+
+class Locations:
+    x = 0
+    y=0
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+class PlayerLocations:
+    puuid=""
+    viewRadians=-1
+    #list of locations
+    locations = []
+    def __init__(self,PlayerLocationsDto):
+        self.puuid = PlayerLocationsDto['puuid']
+        self.viewRadians= PlayerLocationsDto['viewRadians']
+        self.__setlocations(PlayerLocationsDto['locations'])
+    def __setlocations(self,locations):
+        for x in locations:
+            self.locations.append(Locations(locations[x]['x'],locations[x]['y']))
+    
--- matchhandler.py
+++ matchhandler.py
@@ -1,7 +1,32 @@
 import accounthandling
 import getinfo
 import playerClass
+import LocationHandler
+class round:
+    roundnum=-1
+    roundResult=""
+    roundCeremony=""
+    winningTeam=""
+    bombPlanter="" #puuid
+    bombDefuser = "" #puuid
+    plantRoundTime = -1
+    plantPlayerLocations = [] #make this a list of Playerlocations
+    def __init__(self,roundDto):
+        self.roundnum = roundDto['roundNumber']
+        self.roundResult = roundDto['roundResult']
+        self.roundCeremony = roundDto['roundCeremony']
+        self.winningTeam = roundDto['winningTeam']
+        self.bombPlanter = roundDto['bombPlanter']
+        self.bombDefuser = roundDto['bombDefuser']
+        self.plantRoundTime = roundDto['plantRoundTime']
+        self.__setplantPlayerLocations(roundDto['plantPlayerLocations'])
 
+    def __setplantPlayerLocations(self,playerlocations):
+        #assumes roundDto['plantPlayerLocations'] is a list of playerlocations
+        #if not, will need to be changed
+        for x in playerlocations:
+            #need to create PlayerLocation object
+            self.plantPlayerLocations.append(LocationHandler.PlayerLocations(playerlocations[x]))
 
 class match:
     def __init__(self) -> None:
