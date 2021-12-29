48238c46090813804635f85c97272caf270e55d4
Archit Kalla <archkalla@gmail.com>
2021-12-29 14:19:26 -0500

    Update APIWrapper.py

--- APIWrapper.py
+++ APIWrapper.py
@@ -3,7 +3,7 @@ import AccountClass
 
 def main():
     print("This is the main function")
-    apiObject = getinfo.apihandler("RGAPI-dc863e8b-237b-4a8e-b8a3-525bfc57fd1d")
+    apiObject = getinfo.apihandler("")
     accountDto = apiObject.getvalpuuid("ohmygodarchie","001")
     account = AccountClass.Account(accountDto,apiObject)
     print(account.puuid)
