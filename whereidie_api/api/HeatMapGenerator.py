72e04203e7eb5c71e4e720b823a0052e4d923b44
Archit Kalla <archkalla@gmail.com>
2022-01-21 15:58:03 -0600

    fixed mysql to add ids (sequential) and added enums for things

--- whereidie_api/api/HeatMapGenerator.py
+++ whereidie_api/api/HeatMapGenerator.py
@@ -8,6 +8,46 @@ import inspect
 #from django.db import models
 from . import models
 from asgiref.sync import sync_to_async
+from enum import Enum
+class BuyType (Enum):
+    FULL = "Full"
+    HALF = "Half"
+    FORCE = "Force"
+    SAVE = "Save"
+class Map(Enum):
+    ASCENT = "Ascent"
+    BIND = "Bind"
+    BREEZE = "Breeze"
+    FRACTURE = "Fracture"
+    SPLIT = "Split"
+    HAVEN = "Haven"
+    ICEBOX = "Icebox"
+    def __str__(self):
+        return self.value
+class Rank(Enum):
+    pass
+    # 3 -> "Iron 1"
+    # 4 -> "Iron 2"
+    # 5 -> "Iron 3"
+    # 6 -> "Bronze 1"
+    # 7 -> "Bronze 2"
+    # 8 -> "Bronze 3"
+    # 9 -> "Silver 1"
+    # 10 -> "Silver 2"
+    # 11 -> "Silver 3"
+    # 12 -> "Gold 1"
+    # 13 -> "Gold 2"
+    # 14 -> "Gold 3"
+    # 15 -> "Plat 1"
+    # 16 -> "Plat 2"
+    # 17 -> "Plat 3"
+    # 18 -> "Diamond 1"
+    # 19 -> "Diamond 2"
+    # 20 -> "Diamond 3"
+    # 21 -> "Immortal 1"
+    # 22 -> "Immortal 2"
+    # 23 -> "Immortal 3"
+    # 24 -> "Radiant"
 def create_models(db_table):
     classvar = None
     if db_table == "kd_collector_ASCENT":
@@ -35,7 +75,8 @@ def generate_heatmaps():
     for x in Constants.MAPS.values():
         db_table = "kd_collector_"+ x.upper()
         classvar = create_models(db_table)
-        print(type(classvar))
+        if classvar == None:
+            continue
         all_objs =  classvar.all()
         for x in all_objs:
             print(x)
